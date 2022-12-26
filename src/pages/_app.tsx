8d3af3069db14ee7acbb484e005ff64d1702e952
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-26 16:00:19 +0000

    feat: adds inter front through @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from 'next-themes';
 import { type AppType } from 'next/dist/shared/lib/utils';
+import { Inter } from '@next/font/google';
 
 import '../styles/globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <div className={`${inter.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 };
