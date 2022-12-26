4df2c3f2ab1ec531397ca3254c4aa9b87e715054
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-26 14:44:18 +0000

    removes boilerplate

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 80,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
