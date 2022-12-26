97ac601e4924133713058e6e0cfff5356cd476f7
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-26 16:09:05 +0000

    feat: adds title & logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type AppType } from 'next/dist/shared/lib/utils';
 import { Inter } from '@next/font/google';
 
 import '../styles/globals.css';
+import { DefaultSeo } from 'next-seo';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -11,11 +12,17 @@ const inter = Inter({
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class">
-      <div className={`${inter.variable} font-sans`}>
-        <Component {...pageProps} />
-      </div>
-    </ThemeProvider>
+    <>
+      <DefaultSeo
+        title="RHUL Google Dev Club"
+        description="The Royal Holloway Developers Club is a community of curious and open minded individuals interested in technology. The club is focused on taking a more practical and peer-to-peer approach to help bring together many single-minded people while significantly improving their employability."
+      />
+      <ThemeProvider attribute="class">
+        <div className={`${inter.variable} font-sans`}>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
+    </>
   );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/logo-mini.svg" />
+      </Head>
       <body className="bg-white text-black dark:bg-zinc-900 dark:text-white">
         <Main />
         <NextScript />
